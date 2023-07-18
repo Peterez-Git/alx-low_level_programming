66044c817a029c9ecb04673c7403298ba302f970
Peterez-Git <peterez5876@gmail.com>
2023-07-17 19:12:32 -0700

    Fixed again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - A script that prints the sizes of the various data types in C
  *
  * Return: always 0 (success)
  */
@@ -12,6 +11,7 @@ int l;
 long int m;
 long long int n;
 float o;
+
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(k));
 printf("Size of an int: %lu  byte(s)\n", (unsigned long)sizeof(l));
 printf("Size of a long int: %lu  byte(s)\n", (unsigned long)sizeof(m));
