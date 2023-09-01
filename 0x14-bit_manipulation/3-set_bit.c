10bf8d53f770ad7b6a59501650527b31c0adc999
Peterez-Git <peterez5876@gmail.com>
2023-08-31 23:29:29 -0700

    Update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * Peter Anthony's Work
- * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number to change
- * @index: index of the bit to set to 1
+ * Peter Anthony's Pepple's  Work
+ * set_bit - This command sets or establishes a bit at a given index to one
+ * @n: This refers to the pointer to the number to change
+ * @index: Specifies the  index of the bit to set to one
  *
- * Return: 1 for success, -1 for failure
+ * Return: positive one for success, negative one  for failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
